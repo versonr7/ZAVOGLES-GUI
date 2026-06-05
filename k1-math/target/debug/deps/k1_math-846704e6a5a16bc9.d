8df859f9e887c2cb111b6792877805ee65670fff
versonr7 <fastsshadow220@gmail.com>
2026-06-05 16:10:02 +0300

    fix: integrate k1-math as regular directory (remove nested repo)

---
+++ k1-math/target/debug/deps/k1_math-846704e6a5a16bc9.d
@@ -0,0 +1,7 @@
+/data/data/com.termux/files/home/K1-Platform/k1-math/target/debug/deps/k1_math-846704e6a5a16bc9.d: src/lib.rs
+
+/data/data/com.termux/files/home/K1-Platform/k1-math/target/debug/deps/libk1_math-846704e6a5a16bc9.rlib: src/lib.rs
+
+/data/data/com.termux/files/home/K1-Platform/k1-math/target/debug/deps/libk1_math-846704e6a5a16bc9.rmeta: src/lib.rs
+
+src/lib.rs:
